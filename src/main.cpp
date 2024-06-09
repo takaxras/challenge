3bc5003e8620cc0607a6bb91c7511f528a73d79e
takaxras <takaxras@notmyemail.moc>
2024-06-09 21:18:20 +0200

    Refactor and parallel computation
    Split circles drawing from detection. Added corner detector. Using async for parallel corners and circles detection

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
+#include <future>
 #include <opencv2/opencv.hpp>
 #include <opencv2/videoio.hpp>
+#include <opencv2/features2d.hpp>
 
 using namespace cv;
 using namespace std;
 
-void findCircles(Mat& image)
+vector<KeyPoint> findCorners(Mat& image)
 {
-    cout << "Finding circles...\n";
-    Mat imageGrayscale;
+    cout << "Finding corners...\n";
 
+    Mat imageGrayscale;
     cvtColor(image, imageGrayscale, COLOR_BGR2GRAY);
-    GaussianBlur(imageGrayscale, imageGrayscale, Size(3, 3), 2, 2 );
-    vector<Vec3f> circles;
-    HoughCircles(
-        imageGrayscale,
-        circles,
-        HOUGH_GRADIENT,
-        2.,
-        min(imageGrayscale.rows, imageGrayscale.cols)/4.,
-        300.,
-        200.);
-    
+
+    Mat corners;
+    vector<KeyPoint> keypoints_object;
+
+    auto detector = ORB::create();
+    detector->detect(imageGrayscale, keypoints_object);
+
+    return keypoints_object;
+}
+
+void drawCircles(Mat& image, const vector<Vec3f>& circles)
+{
     const auto nCircles = circles.size();
     cout << nCircles << " detected\n";
 
@@ -29,14 +32,32 @@ void findCircles(Mat& image)
     {
         Point center(cvRound(c[0]), cvRound(c[1]));
         const auto radius = cvRound(c[2]);
-        
+
         // draw the circle center
         circle(image, center, 3, Scalar(0,255,0), -1, 8, 0);
         // draw the circle outline
         circle(image, center, radius, Scalar(0,0,255), 3, 8, 0);
     }
+}
 
-    imshow("Circles", image);
+vector<Vec3f> findCircles(Mat& image)
+{
+    cout << "Finding circles...\n";
+    Mat imageGrayscale;
+    cvtColor(image, imageGrayscale, COLOR_BGR2GRAY);
+    Mat imageBlurred;
+    GaussianBlur(imageGrayscale, imageBlurred, Size(3, 3), 2, 2 );
+    vector<Vec3f> circles;
+    HoughCircles(
+        imageBlurred,
+        circles,
+        HOUGH_GRADIENT,
+        2.,
+        min(image.rows, image.cols)/4.,
+        150.,
+        200.);
+
+    return circles;
 }
 
 bool processCam()
@@ -65,7 +86,13 @@ bool processCam()
             break;
         }
 
-        findCircles(frame);
+        auto f1 = async(findCircles,ref(frame));
+        auto f2 = async(findCorners,ref(frame));
+
+        drawCircles(frame, f1.get());
+        drawKeypoints(frame, f2.get(), frame, cv::Scalar(0,255,255));
+        imshow("Boom", frame);
+
         if (waitKey(5) >= 0)
             break;
     }
